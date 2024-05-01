629e747e85dfb0d5006aec3bedeb4b152633aaa4
i7modes <mohammadbazns@gmail.com>
2024-05-01 22:47:45 +0300

    Update main.c
    following code convention

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node* TNode;
+typedef struct Node *TNode;
 
-struct Node {
+struct Node
+{
 
     int Element;
     struct Node *Left;
@@ -17,48 +18,59 @@ TNode FindMax(TNode);
 TNode Insert(int, TNode);
 TNode Delete(int, TNode);
 
-int main(){
+int main()
+{
     TNode myTree = MakeEmpty(NULL);
     myTree = Insert(2, myTree);
     myTree = Insert(1, myTree);
     myTree = Insert(3, myTree);
 
-    if(FindMin(myTree) == NULL){
+    if (FindMin(myTree) == NULL)
+    {
         printf("Empty Tree!\n");
-    } else {
+    }
+    else
+    {
         printf("Min Number: %d\n", FindMin(myTree)->Element);
-
     }
 
-    if(FindMax(myTree) == NULL){
+    if (FindMax(myTree) == NULL)
+    {
         printf("Empty Tree!\n");
-    } else {
+    }
+    else
+    {
         printf("Max Number Before Deletion: %d\n", FindMax(myTree)->Element);
-
     }
 
     myTree = Delete(3, myTree);
 
-    if(FindMax(myTree) == NULL){
+    if (FindMax(myTree) == NULL)
+    {
         printf("Empty Tree!\n");
-    } else {
+    }
+    else
+    {
         printf("Max Number After Deletion: %d\n", FindMax(myTree)->Element);
-
     }
 
-    if(Find(2, myTree) == NULL){
+    if (Find(2, myTree) == NULL)
+    {
         printf("Empty Tree!\n");
-    } else {
+    }
+    else
+    {
         printf("Left element of element 2: %d\n", Find(2, myTree)->Left->Element);
-
     }
 
     printf("\nDone!\n");
     return 0;
 }
 
-TNode MakeEmpty(TNode T){
-    if (T != NULL) {
+TNode MakeEmpty(TNode T)
+{
+    if (T != NULL)
+    {
         MakeEmpty(T->Left);
         MakeEmpty(T->Right);
         free(T);
@@ -67,31 +79,39 @@ TNode MakeEmpty(TNode T){
     return NULL;
 }
 
-TNode Find(int X, TNode T){
-    if (T == NULL) {
+TNode Find(int X, TNode T)
+{
+    if (T == NULL)
+    {
         return NULL;
-
-    } else if (X < T->Element){
+    }
+    else if (X < T->Element)
+    {
         return Find(X, T->Left);
-
-    } else if (X > T->Element){
+    }
+    else if (X > T->Element)
+    {
         return Find(X, T->Right);
-
-    } else {
+    }
+    else
+    {
         return T;
     }
 }
 
-TNode FindMin(TNode T){
-    if (T == NULL) {
+TNode FindMin(TNode T)
+{
+    if (T == NULL)
+    {
         return NULL;
-
-    } else if (T->Left == NULL){
+    }
+    else if (T->Left == NULL)
+    {
         return T;
-
-    } else {
+    }
+    else
+    {
         return FindMin(T->Left);
-
     }
 }
 
@@ -108,15 +128,17 @@ TNode FindMin(TNode T){
 
 TNode FindMax(TNode T)
 {
-    if (T == NULL) {
+    if (T == NULL)
+    {
         return NULL;
-
-    } else if (T->Right == NULL) {
+    }
+    else if (T->Right == NULL)
+    {
         return T;
-
-    } else {
+    }
+    else
+    {
         return FindMax(T->Right);
-
     }
 }
 
@@ -131,58 +153,71 @@ TNode FindMax(TNode T){
 }
 */
 
-TNode Insert(int X, TNode T){
-    if (T == NULL){
+TNode Insert(int X, TNode T)
+{
+    if (T == NULL)
+    {
 
         T = (struct Node *)malloc(sizeof(struct Node));
 
-        if (T == NULL){
+        if (T == NULL)
+        {
             printf("Out of space");
             exit(1);
-
-        } else {
+        }
+        else
+        {
             T->Element = X;
             T->Left = T->Right = NULL;
         }
-
-    } else if (X < T->Element){
+    }
+    else if (X < T->Element)
+    {
         T->Left = Insert(X, T->Left);
-
-    } else if (X > T->Element){
+    }
+    else if (X > T->Element)
+    {
         T->Right = Insert(X, T->Right);
-
     }
 
     return T;
 }
 
-TNode Delete(int X, TNode T){
+TNode Delete(int X, TNode T)
+{
     TNode TmpCell;
 
-    if (T == NULL){
+    if (T == NULL)
+    {
         printf("Element not found");
-
-    } else if (X < T->Element){
+    }
+    else if (X < T->Element)
+    {
         T->Left = Delete(X, T->Left);
-
-    } else if (X > T->Element){
+    }
+    else if (X > T->Element)
+    {
         T->Right = Delete(X, T->Right);
-
-    } else {
-        if (T->Left && T->Right){
+    }
+    else
+    {
+        if (T->Left && T->Right)
+        {
             TmpCell = FindMin(T->Right);
             T->Element = TmpCell->Element;
             T->Right = Delete(T->Element, T->Right);
-
-        } else {
+        }
+        else
+        {
             TmpCell = T;
 
-            if (T->Left == NULL){
+            if (T->Left == NULL)
+            {
                 T = T->Right;
-
-            } else if (T->Right == NULL){
+            }
+            else if (T->Right == NULL)
+            {
                 T = T->Left;
-
             }
             free(TmpCell);
         }
